d86bc5c39117595fb748391bb5982ed9162fa2a6
James Coleman <coleman1@gmail.com>
2019-06-21 10:08:58 -0400

    improved touchpad control

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,11 +19,12 @@ public class Player : MonoBehaviour
     public GameObject jumpFaceVar;
     public GameObject idleFaceVar;
 
-    private MLInputController _controller;
+    private MLInputController _mlController;
+    public Camera mainCamera;
 
     void Awake()
     {
-        _controller = MLInput.GetController(MLInput.Hand.Left);
+        _mlController = MLInput.GetController(MLInput.Hand.Left);
     }
 
     // Start is called before the first frame update
@@ -34,6 +35,7 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
+    /* Old Update Method - Touch pad like keyboard arrows
     void Update()
     {
         if(controller.isGrounded && _controller.Touch1PosAndForce.z > 0.0f)
@@ -73,6 +75,122 @@ public class Player : MonoBehaviour
         controller.Move(moveDirection * Time.deltaTime);
         moveDirection.y -= gravity * Time.deltaTime; 
         
+    }*/
+
+
+    /*old update 2 direction relative to self
+
+    void Update()
+{
+    if (controller.isGrounded && _controller.Touch1PosAndForce.z > 0.0f)
+    {
+        anim.SetInteger("AnimPar", 1);
+
+        Vector3 touchpadDirection = new Vector3(_controller.Touch1PosAndForce.x, 0, _controller.Touch1PosAndForce.y);
+        Quaternion touchpadRotation = Quaternion.LookRotation(touchpadDirection);
+
+        //moveDirection = touchpadDirection * _controller.Touch1PosAndForce.y * speed;
+        moveDirection = touchpadDirection * speed;
+        //float turn = _controller.Touch1PosAndForce.x;
+        //TODO try to remove rotation so that figure moves relative to world space
+        transform.rotation = touchpadRotation;
+        //transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+
+        runFaceVar.SetActive(true);
+        idleFaceVar.SetActive(false);
+        jumpFaceVar.SetActive(false);
+
     }
+    else if (controller.isGrounded)
+    {
+        anim.SetInteger("AnimPar", 0);
+
+        Vector3 touchpadDirection = new Vector3(_controller.Touch1PosAndForce.x, 0, _controller.Touch1PosAndForce.y);
+        Quaternion touchpadRotation = Quaternion.LookRotation(touchpadDirection);
+
+        //moveDirection = touchpadDirection * _controller.Touch1PosAndForce.y * speed;
+        moveDirection = touchpadDirection * speed * 0;
+        //float turn = _controller.Touch1PosAndForce.x;
+        transform.rotation = touchpadRotation;
+        //transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+
+        runFaceVar.SetActive(false);
+        idleFaceVar.SetActive(true);
+        jumpFaceVar.SetActive(false);
+    }
+
+    if (controller.isGrounded && _controller.TriggerValue > 0.2f)
+    {
+        anim.SetInteger("AnimPar", 2);
+        moveDirection.y = jumpForce;
+
+        runFaceVar.SetActive(false);
+        idleFaceVar.SetActive(false);
+        jumpFaceVar.SetActive(true);
+    }
+
+    controller.Move(moveDirection * Time.deltaTime);
+    moveDirection.y -= gravity * Time.deltaTime;
+
+}
+
+
+
+}
+
+     */
+
+
+    void Update()
+    {
+
+        float X = _mlController.Touch1PosAndForce.x;
+        float Y = _mlController.Touch1PosAndForce.y;
+        Vector3 touchpadDirection = new Vector3(_mlController.Touch1PosAndForce.x, 0, _mlController.Touch1PosAndForce.y);
+
+        if (controller.isGrounded && _mlController.Touch1PosAndForce.z > 0.0f)
+        {
+            anim.SetInteger("AnimPar", 1);
+
+            Vector3 forward = Vector3.Normalize(Vector3.ProjectOnPlane(mainCamera.transform.forward, Vector3.up));
+            Vector3 right = Vector3.Normalize(Vector3.ProjectOnPlane(mainCamera.transform.right, Vector3.up));
+            moveDirection = Vector3.Normalize((X * right) + (Y * forward)) * speed;
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+
+            runFaceVar.SetActive(true);
+            idleFaceVar.SetActive(false);
+            jumpFaceVar.SetActive(false);
+
+        }
+        else if (controller.isGrounded)
+        {
+            anim.SetInteger("AnimPar", 0);
+
+            Vector3 forward = Vector3.Normalize(Vector3.ProjectOnPlane(mainCamera.transform.forward, Vector3.up));
+            Vector3 right = Vector3.Normalize(Vector3.ProjectOnPlane(mainCamera.transform.right, Vector3.up));
+            moveDirection = Vector3.Normalize((X * right) + (Y * forward)) * speed * 0;
+            //transform.rotation = Quaternion.LookRotation(moveDirection);
+
+            runFaceVar.SetActive(false);
+            idleFaceVar.SetActive(true);
+            jumpFaceVar.SetActive(false);
+        }
+
+        if (controller.isGrounded && _mlController.TriggerValue > 0.2f)
+        {
+            anim.SetInteger("AnimPar", 2);
+            moveDirection.y = jumpForce;
+
+            runFaceVar.SetActive(false);
+            idleFaceVar.SetActive(false);
+            jumpFaceVar.SetActive(true);
+        }
+        
+        controller.Move(moveDirection * Time.deltaTime);
+        moveDirection.y -= gravity * Time.deltaTime;
+
+    }
+
+
     
 }
